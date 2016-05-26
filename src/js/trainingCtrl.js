31139f3d1b73f4abcbe7b01936ab53966e0030c2
LONON2 <oliver.nural@paconsulting.com>
2016-05-26 13:54:45 +0100

    added training page

--- src/js/trainingCtrl.js
+++ src/js/trainingCtrl.js
@@ -3,12 +3,63 @@
 
   angular.module('app').controller('trainingCtrl', TrainingCtrl);
 
-  function TrainingCtrl() {
+  function TrainingCtrl($rootScope) {
     var vm = this;
+    $rootScope.isTraining = true;    
+    var div = $('#training-point')
 
     vm.message = 'training';
 
+    setTimeout(function() {
+      console.log("First");
+      $('#training-point').css(
+        { right: '10%' }
+      );
+      moveDiv();
+    }, 3000);
 
+    function moveDiv(){
+      setTimeout(function() {
+        console.log("Second");
+        $('#training-point').css(
+          { top: '90%' }
+        );
+        moveDiv2();
+      }, 3000);
+    }
+
+    function moveDiv2(){
+      setTimeout(function() {
+        console.log("Third");
+        $('#training-point').css(
+          { left: '10%' }
+        );
+        moveDiv3();
+      }, 3000);
+    }
+
+    function moveDiv3(){
+      setTimeout(function() {
+        console.log("Fourth");
+        $('#training-point').css(
+          { top: '50%', left: '50%' }
+        );
+        finishTraining();
+      }, 3000);
+    }
+
+    function finishTraining() {
+      setTimeout(function() {
+        console.log("Finised Training");
+        $rootScope.isTraining = true;      
+      });
+    }
+
+    // $timeout(function() {
+    //   div.css({
+    //     "left": "500px"
+    //   })
+    // }, 10);
 
     // webgazer.begin();
 
